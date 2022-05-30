45c66e51759dd6ab964c6cbffce9a809ae6f5e4c
xanderstuff <68503800+xanderstuff@users.noreply.github.com>
2022-05-30 14:40:51 -0500

    Don't render Overlays if the debug hud or ProfileEditorScreen is open, to reduce clutter

--- src/main/java/io/github/xanderstuff/ultimatehud/mixin/hud/InGameHudMixin.java
+++ src/main/java/io/github/xanderstuff/ultimatehud/mixin/hud/InGameHudMixin.java
@@ -20,19 +20,27 @@ public class InGameHudMixin {
 
     @Inject(at = @At("HEAD"), method = "render")
     private void ultimatehud$renderPre(MatrixStack matrixStack, float tickDelta, CallbackInfo info) {
-        if (!client.options.hudHidden) {
+        var isEditorScreenOpen = client.currentScreen instanceof ProfileEditorScreen;
+
+        if (!client.options.hudHidden && !isEditorScreenOpen) {
             HudManager.renderBelowHudOverlays(matrixStack, tickDelta, client.player);
         }
 
-        if (client.currentScreen instanceof ProfileEditorScreen) {
+        if (isEditorScreenOpen) {
             ((ProfileEditorScreen) client.currentScreen).drawBackground(matrixStack);
         }
     }
 
+    //FIXME: RETURN is after the debug hud (F3), but HUD stuff should be rendered before the debug hud (we don't want to cover it)
     @Inject(at = @At("RETURN"), method = "render")
     private void ultimatehud$renderPost(MatrixStack matrixStack, float tickDelta, CallbackInfo info) {
+        var isEditorScreenOpen = client.currentScreen instanceof ProfileEditorScreen;
+
         if (!client.options.hudHidden) {
             HudManager.renderWidgets(matrixStack, tickDelta, client.player);
+        }
+
+        if (!client.options.hudHidden && !isEditorScreenOpen && !client.options.debugEnabled) {
             HudManager.renderAboveHudOverlays(matrixStack, tickDelta, client.player);
         }
     }
