5e676b15e556e6624b0b8ec2a7dceff521235431
xanderstuff <68503800+xanderstuff@users.noreply.github.com>
2023-01-04 08:53:14 -0600

    Cleanup/simplify ArmourIndicatorWidgetMixin

--- src/main/java/io/github/xanderstuff/ultimatehud/mixin/hud/widgets/ArmourIndicatorWidgetMixin.java
+++ src/main/java/io/github/xanderstuff/ultimatehud/mixin/hud/widgets/ArmourIndicatorWidgetMixin.java
@@ -4,7 +4,6 @@ import io.github.xanderstuff.ultimatehud.hud.widgets.minecraft.ArmourIndicatorWi
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,15 +24,15 @@ public abstract class ArmourIndicatorWidgetMixin {
 
     @Inject(method = "renderStatusBars", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;push(Ljava/lang/String;)V", args = "ldc=armor"))
     private void ultimatehud$ArmourIndicatorWidgetMixin$renderPre(MatrixStack matrixStack, CallbackInfo ci) {
-        //TODO: this can definitely be simplified
-        float maxHealth = Math.max((float) client.player.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH), (float) MathHelper.ceil(client.player.getHealth()));
-        int absorption = MathHelper.ceil(client.player.getAbsorptionAmount());
-        int heartRows = MathHelper.ceil((maxHealth + (float) absorption) / 2.0F / 10.0F);
-        int rowHeight = Math.max(10 - (heartRows - 2), 3);
+        float maxHealth = MathHelper.ceil(Math.max(client.player.getHealth(), client.player.getMaxHealth()));
+        float absorption = MathHelper.ceil(client.player.getAbsorptionAmount());
 
+        int heartRows = MathHelper.ceil((maxHealth + absorption) / 20.0F);
+        int rowHeight = Math.max(12 - heartRows, 3);
+        int healthOffset = (heartRows - 1) * rowHeight;
 
         int originalX = scaledWidth / 2 - 91;
-        int originalY = scaledHeight - 49 - (heartRows - 1) * rowHeight;
+        int originalY = scaledHeight - 49 - healthOffset;
         int newX = (int) ArmourIndicatorWidget.getInstance().cachedPosition.x - originalX;
         int newY = (int) ArmourIndicatorWidget.getInstance().cachedPosition.y - originalY;
 
